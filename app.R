37d5fd1f451b2e1d6c3ae000d0e70c1ad92f0d3d
saxasm <vilhelm.agdur@gmail.com>
2023-03-22 13:45:37 +0100

    start creating app
    it can now plot a planted clique model graph - next up, adding colouring by ground truth and various attempts at finding it

--- app.R
+++ app.R
@@ -8,43 +8,57 @@
 #
 
 library(shiny)
+library(igraph)
+
+source("generate_planted_clique.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Recovery of the planted clique"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            sliderInput("n",
+                        "Number of vertices:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 100,
+                        round = TRUE,
+                        value = 30),
+            sliderInput("k",
+                        "Size of planted clique:",
+                        min = 1,
+                        max = 100,
+                        round = TRUE,
+                        value = 5),
+            sliderInput("p",
+                        "Probability of edges within clique:",
+                        min = 0,
+                        max = 1,
+                        value = 1),
+            sliderInput("q",
+                        "Probability of edges not within clique:",
+                        min = 0,
+                        max = 1,
+                        value = 0.3)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("graphPlot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  planted_clique_graph <- reactive(generate_planted_clique(input$n, input$k, input$p, input$q))
+  
+  output$graphPlot <- renderPlot({
+      plot.igraph(planted_clique_graph()$graph)
+  })
 }
 
 # Run the application 
