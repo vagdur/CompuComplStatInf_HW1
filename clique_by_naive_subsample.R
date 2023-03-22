94dddd48155f8ed602761a419c0710b4cc0e8e4e
saxasm <vilhelm.agdur@gmail.com>
2023-03-22 13:21:15 +0100

    fix AI code
    It made one function for sampling up and made one minor error

--- clique_by_naive_subsample.R
+++ clique_by_naive_subsample.R
@@ -4,10 +4,10 @@ clique_by_naive_subsample <- function(G, k, p) {
   
   # Step 2
   vertex_probabilities <- degree(G)/(2*m) + p
-  H <- sample_v(graph=G, size=vcount(G), prob=vertex_probabilities, replace=TRUE)
+  H <- V(t$graph)[runif(length(V(t$graph))) < vertex_probabilities]
   
   # Step 3
-  G_prime <- induced_subgraph(G, V(H))
+  G_prime <- induced_subgraph(G, H)
   
   # Step 4
   top_k_vertices <- head(order(degree(G_prime), decreasing=TRUE), k)
