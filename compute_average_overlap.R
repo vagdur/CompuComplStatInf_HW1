fe79ae83d6e871a288e8e36053d583dcd7e4cbdc
saxasm <vilhelm.agdur@gmail.com>
2023-03-22 11:59:10 +0100

    tweak AI code
    Forgot to tell it that find_clique needs to know k, and intentionally gave it the wrong name for generate_planted_clique

--- compute_average_overlap.R
+++ compute_average_overlap.R
@@ -5,12 +5,12 @@ compute_average_overlap <- function(find_clique, N, n, k, p, q) {
   # Generate N random graphs and compute overlap for each graph
   for (i in 1:N) {
     # Generate a random graph using the generate_graph function
-    graph_and_S <- generate_graph(n, k, p, q)
+    graph_and_S <- generate_planted_clique(n, k, p, q)
     graph <- graph_and_S[[1]]
     S <- graph_and_S[[2]]
     
     # Find the clique using the find_clique function
-    S_hat <- find_clique(graph)
+    S_hat <- find_clique(graph, k)
     
     # Compute the overlap between S_hat and the actual set S
     overlap <- length(intersect(S, S_hat)) / k
