0fca70eef96151aa2e0ac8e33da7afed356070a3
saxasm <vilhelm.agdur@gmail.com>
2023-03-22 11:56:47 +0100

    Create compute_average_overlap.R
    Initial ChatGPT prompt, that gives a bad response:
    
    "Could you generate code to do the following:
    
    1. Given a function find_clique and parameters N,n, k, p, and q, generate N random graphs using the function generate_graph(n, k, p, q), which we created earlier
    2. For each of the N graphs, run the function find_clique on them, which returns a set S_hat
    3. Compute the size of the intersection of S_hat and the actual set S for the graph, and divide this by k - call this the overlap of the graph
    4. Return the average overlap among the graphs we generated"
    
    Then it returns this when prompted with:
    
    "The code you generated contains a problem: The function generate_graph returns a list, the first element of which is the graph, and the second element of which is S. This S is the one we want to intersect with S_hat."

---
+++ compute_average_overlap.R
@@ -0,0 +1,27 @@
+compute_average_overlap <- function(find_clique, N, n, k, p, q) {
+  # Initialize total overlap to 0
+  total_overlap <- 0
+  
+  # Generate N random graphs and compute overlap for each graph
+  for (i in 1:N) {
+    # Generate a random graph using the generate_graph function
+    graph_and_S <- generate_graph(n, k, p, q)
+    graph <- graph_and_S[[1]]
+    S <- graph_and_S[[2]]
+    
+    # Find the clique using the find_clique function
+    S_hat <- find_clique(graph)
+    
+    # Compute the overlap between S_hat and the actual set S
+    overlap <- length(intersect(S, S_hat)) / k
+    
+    # Add the overlap to the total
+    total_overlap <- total_overlap + overlap
+  }
+  
+  # Compute the average overlap among the graphs
+  avg_overlap <- total_overlap / N
+  
+  # Return the average overlap
+  return(avg_overlap)
+}
