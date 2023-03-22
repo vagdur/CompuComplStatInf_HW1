4bcb45b07470a6e309d155dae817c01cf526adf7
saxasm <vilhelm.agdur@gmail.com>
2023-03-22 13:44:18 +0100

    tweak algorithm
    It still doesn't really work for finding the clique but I guess that is okay

--- clique_by_naive_subsample.R
+++ clique_by_naive_subsample.R
@@ -1,15 +1,15 @@
-clique_by_naive_subsample <- function(G, k, p) {
-  # Step 1
-  m <- ecount(G)
+clique_by_naive_subsample <- function(G, k, p, gamma) {
+  # Step 1: Find probabilities of including each vertex in our subsample:
+  vertex_percentiles <- rank(degree(G), ties.method="min")/vcount(G)
+  vertex_probabilities <- (vertex_percentiles + p)**gamma
   
-  # Step 2
-  vertex_probabilities <- degree(G)/(2*m) + p
+  # Step 2: Subsample
   H <- V(t$graph)[runif(length(V(t$graph))) < vertex_probabilities]
   
-  # Step 3
+  # Step 3: Induced subgraph on our subsample
   G_prime <- induced_subgraph(G, H)
   
-  # Step 4
+  # Step 4: Degree test on the subsampled graph
   top_k_vertices <- head(order(degree(G_prime), decreasing=TRUE), k)
   
   return(V(G_prime)[top_k_vertices])
